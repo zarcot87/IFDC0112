b8115f23fa075a518e522feddd06ea76e2c5adc5
Erik Tarruella <eriktarruella@gmail.com>
2025-01-29 10:03:42 +0000

    fix: typo

--- assembler/nasm-arch/initial.bash
+++ assembler/nasm-arch/initial.bash
@@ -1,11 +1,11 @@
 # Compilar el programa con NASM
-nasm -f elf64 -o programa.o programa.asm
+nasm -f elf64 -o programa.o inital.asm
 
 # Enlazar el archivo objeto con LD para crear el ejecutable
-ld -s -o programa programa.o
+ld -s -o inital inital.o
 
 # Ejecutar el programa
-./programa
+./inital
 
 # Imprimir el código de salida del programa
 echo "Código de salida: $?"
