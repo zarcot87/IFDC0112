3ceebf37c68c790f9aee1d27deb0445f5f74047b
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-02-02 12:28:36 +0000

    time sort command

--- EDA/cars/sorting_time.sh
+++ EDA/cars/sorting_time.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
  
-/usr/bin/time -f "00-->%e %U %S" sort one_million.csv > /dev/null
-#/usr/bin/time -f "01-->%e %U %S" sort --parallel=01 one_million.csv > /dev/null
-#/usr/bin/time -f "02-->%e %U %S" sort --parallel=02 one_million.csv > /dev/null
-#/usr/bin/time -f "03-->%e %U %S" sort --parallel=03 one_million.csv > /dev/null
-#/usr/bin/time -f "04-->%e %U %S" sort --parallel=04 one_million.csv > /dev/null
-#/usr/bin/time -f "05-->%e %U %S" sort --parallel=05 one_million.csv > /dev/null
-#/usr/bin/time -f "06-->%e %U %S" sort --parallel=06 one_million.csv > /dev/null
-#/usr/bin/time -f "07-->%e %U %S" sort --parallel=07 one_million.csv > /dev/null
-#/usr/bin/time -f "08-->%e %U %S" sort --parallel=08 one_million.csv > /dev/null
-#/usr/bin/time -f "09-->%e %U %S" sort --parallel=09 one_million.csv > /dev/null
-/usr/bin/time -f "10-->%e %U %S" sort --parallel=10 one_million.csv > /dev/null
-/usr/bin/time -f "20-->%e %U %S" sort --parallel=20 one_million.csv > /dev/null
-/usr/bin/time -f "30-->%e %U %S" sort --parallel=30 one_million.csv > /dev/null
-/usr/bin/time -f "40-->%e %U %S" sort --parallel=40 one_million.csv > /dev/null
-/usr/bin/time -f "50-->%e %U %S" sort --parallel=50 one_million.csv > /dev/null
+/usr/bin/time -f "00-->%e Sec   %U Sec   %S Sec  " sort one_million.csv > /dev/null &
+/usr/bin/time -f "01-->%e Sec   %U Sec   %S Sec  " sort --parallel=01 one_million.csv > /dev/null &
+/usr/bin/time -f "02-->%e Sec   %U Sec   %S Sec  " sort --parallel=02 one_million.csv > /dev/null &
+/usr/bin/time -f "03-->%e Sec   %U Sec   %S Sec  " sort --parallel=03 one_million.csv > /dev/null &
+/usr/bin/time -f "04-->%e Sec   %U Sec   %S Sec  " sort --parallel=04 one_million.csv > /dev/null &
+/usr/bin/time -f "05-->%e Sec   %U Sec   %S Sec  " sort --parallel=05 one_million.csv > /dev/null &
+/usr/bin/time -f "06-->%e Sec   %U Sec   %S Sec  " sort --parallel=06 one_million.csv > /dev/null &
+/usr/bin/time -f "07-->%e Sec   %U Sec   %S Sec  " sort --parallel=07 one_million.csv > /dev/null &
+/usr/bin/time -f "08-->%e Sec   %U Sec   %S Sec  " sort --parallel=08 one_million.csv > /dev/null &
+/usr/bin/time -f "09-->%e Sec   %U Sec   %S Sec  " sort --parallel=09 one_million.csv > /dev/null &
+/usr/bin/time -f "10-->%e Sec   %U Sec   %S Sec  " sort --parallel=10 one_million.csv > /dev/null &
+/usr/bin/time -f "20-->%e Sec   %U Sec   %S Sec  " sort --parallel=20 one_million.csv > /dev/null &
+/usr/bin/time -f "30-->%e Sec   %U Sec   %S Sec  " sort --parallel=30 one_million.csv > /dev/null &
+/usr/bin/time -f "40-->%e Sec   %U Sec   %S Sec  " sort --parallel=40 one_million.csv > /dev/null &
+/usr/bin/time -f "50-->%e Sec   %U Sec   %S Sec  " sort --parallel=50 one_million.csv > /dev/null &
  
