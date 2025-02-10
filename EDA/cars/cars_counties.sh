98d19eb2eb4f3936cbc8691148ead9ae95f4abb5
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-02-10 12:41:14 +0000

    Process normalizción tabla cars

---
+++ EDA/cars/cars_counties.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+sed -i -e 's/,Adams,/,1,/' cars_cities.csv 
+sed -i -e 's/,Asotin,/,2,/' cars_cities.csv 
+sed -i -e 's/,Benton,/,3,/' cars_cities.csv 
+sed -i -e 's/,Chelan,/,4,/' cars_cities.csv 
+sed -i -e 's/,Clallam,/,5,/' cars_cities.csv 
+sed -i -e 's/,Clark,/,6,/' cars_cities.csv 
+sed -i -e 's/,Columbia,/,7,/' cars_cities.csv 
+sed -i -e 's/,Cowlitz,/,8,/' cars_cities.csv 
+sed -i -e 's/,Douglas,/,9,/' cars_cities.csv 
+sed -i -e 's/,Ferry,/,10,/' cars_cities.csv 
+sed -i -e 's/,Franklin,/,11,/' cars_cities.csv 
+sed -i -e 's/,Garfield,/,12,/' cars_cities.csv 
+sed -i -e 's/,Grant,/,13,/' cars_cities.csv 
+sed -i -e 's/,Grays Harbor,/,14,/' cars_cities.csv 
+sed -i -e 's/,Island,/,15,/' cars_cities.csv 
+sed -i -e 's/,Jefferson,/,16,/' cars_cities.csv 
+sed -i -e 's/,King,/,17,/' cars_cities.csv 
+sed -i -e 's/,Kitsap,/,18,/' cars_cities.csv 
+sed -i -e 's/,Kittitas,/,19,/' cars_cities.csv 
+sed -i -e 's/,Klickitat,/,20,/' cars_cities.csv 
+sed -i -e 's/,Lewis,/,21,/' cars_cities.csv 
+sed -i -e 's/,Lincoln,/,22,/' cars_cities.csv 
+sed -i -e 's/,Madison,/,23,/' cars_cities.csv 
+sed -i -e 's/,Mason,/,24,/' cars_cities.csv 
+sed -i -e 's/,Okanogan,/,25,/' cars_cities.csv 
+sed -i -e 's/,Pacific,/,26,/' cars_cities.csv 
+sed -i -e 's/,Pend Oreille,/,27,/' cars_cities.csv 
+sed -i -e 's/,Pierce,/,28,/' cars_cities.csv 
+sed -i -e 's/,San Diego,/,29,/' cars_cities.csv 
+sed -i -e 's/,San Juan,/,30,/' cars_cities.csv 
+sed -i -e 's/,Skagit,/,31,/' cars_cities.csv 
+sed -i -e 's/,Skamania,/,32,/' cars_cities.csv 
+sed -i -e 's/,Snohomish,/,33,/' cars_cities.csv 
+sed -i -e 's/,Spokane,/,34,/' cars_cities.csv 
+sed -i -e 's/,Stafford,/,35,/' cars_cities.csv 
+sed -i -e 's/,Stevens,/,36,/' cars_cities.csv 
+sed -i -e 's/,Thurston,/,37,/' cars_cities.csv 
+sed -i -e 's/,Wahkiakum,/,38,/' cars_cities.csv 
+sed -i -e 's/,Walla Walla,/,39,/' cars_cities.csv 
+sed -i -e 's/,Whatcom,/,40,/' cars_cities.csv 
+sed -i -e 's/,Whitman,/,41,/' cars_cities.csv 
+sed -i -e 's/,Williamson,/,42,/' cars_cities.csv 
+sed -i -e 's/,Yakima,/,43,/' cars_cities.csv 
---
+++ EDA/cars/cars_states.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+sed -i -e 's/88/1/' cars_counties.csv 
+sed -i -e 's/CA/2/' cars_counties.csv 
+sed -i -e 's/IL/3/' cars_counties.csv 
+sed -i -e 's/VA/4/' cars_counties.csv 
+sed -i -e 's/WA/5/' cars_counties.csv 
