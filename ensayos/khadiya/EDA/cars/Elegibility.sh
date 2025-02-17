62d2d170296fd91f6f20278219f2171c0afcfcd2
KhadijaBagani <bagani2000@gmail.com>
2025-02-17 11:42:39 +0000

    Ejercicio de normalización

---
+++ ensayos/khadiya/EDA/cars/Elegibility.sh
@@ -0,0 +1,3 @@
+sed -i -e 's/Clean Alternative Fuel Vehicle Eligible/1/' cars01.csv
+sed -i -e 's/Eligibility unknown as battery range has not been researched/2/' cars01.csv
+sed -i -e 's/Not eligible due to low battery range/3/' cars01.csv
